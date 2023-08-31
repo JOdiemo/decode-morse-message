3b954a990fa10ba2c4a60efb796d533a1e4646fd
JOdiemo <odiemo@gmail.com>
2023-08-31 22:13:08 +0300

    Add char and word methods

---
+++ morsecode.rb
@@ -0,0 +1,41 @@
+# rubocop:disable Metrics/MethodLength
+def decode_char(x)
+  morse_char = { '.-' => 'A',
+                 '-...' => 'B',
+                 '-.-.' => 'C',
+                 '-..' => 'D',
+                 '.' => 'E',
+                 '..-.' => 'F',
+                 '--.' => 'G',
+                 '....' => 'H',
+                 '..' => 'I',
+                 '.---' => 'J',
+                 '-.-' => 'K',
+                 '.-..' => 'L',
+                 '--' => 'M',
+                 '-.' => 'N',
+                 '---' => 'O',
+                 '.--.' => 'P',
+                 '--.-' => 'Q',
+                 '.-.' => 'R',
+                 '...' => 'S',
+                 '-' => 'T',
+                 '..-' => 'U',
+                 '...-' => 'V',
+                 '.--' => 'W',
+                 '-..-' => 'X',
+                 '-.--' => 'Y',
+                 '--..' => 'Z' }
+  morse_char[x]
+end
+
+def decode_word(word)
+  morse_word = ' '
+  word.split(" ").each do |item|
+    morse_word = "#{morse_word}#{decode_char(item)}"
+  end
+  morse_word
+end
+
+puts decode_char('.-')
+puts decode_word("-- -.--")
